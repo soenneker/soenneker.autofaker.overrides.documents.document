cd964d45332748418241cea9b716a0921c518e8a
Jake Soenneker <jake@soeneker.com>
2024-12-23 17:53:16 +0000

    Tests

--- test/Soenneker.AutoFaker.Overrides.Documents.Document.Tests/DocumentOverrideTests.cs
+++ test/Soenneker.AutoFaker.Overrides.Documents.Document.Tests/DocumentOverrideTests.cs
@@ -1,5 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
 namespace Soenneker.AutoFaker.Overrides.Documents.Document.Tests;
 
-public class DocumentOverrideTests
+[Collection("Collection")]
+public class DocumentOverrideTests : FixturedUnitTest
 {
+    public DocumentOverrideTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
 }
